0af0f2d4c15555fb613950bc67a00764fcf6d13a
Sergio Sanchez <92520473+sergiosg1504@users.noreply.github.com>
2026-05-14 13:34:25 +0200

    chore: initialize project

---
+++ app/src/main/java/com/backend_dev_test/similar_products/SimilarProductsApplication.java
@@ -0,0 +1,13 @@
+package com.backend_dev_test.similar_products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimilarProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimilarProductsApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/backend_dev_test/similar_products/SimilarProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend_dev_test.similar_products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimilarProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
